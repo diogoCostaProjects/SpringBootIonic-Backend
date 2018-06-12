bc8e5d4a326182da073aa910ab12482651e825fd
diogoCostaProjects <diogocosta.jar@gmail.com>
2018-06-11 21:58:32 -0300

    Primeira classe de dominio criada e retornando JSON

--- src/main/java/com/diogocosta/cursospringionic/resources/CategoriaResource.java
+++ src/main/java/com/diogocosta/cursospringionic/resources/CategoriaResource.java
@@ -1,18 +1,33 @@
 package com.diogocosta.cursospringionic.resources;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.diogocosta.cursospringionic.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
+	public List<Categoria> listar(){
 		
-		return "Rest Testado !!! Graças a God";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		Categoria cat3 = new Categoria(3,"Outros");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		
+		return lista;
 	}
 
 }

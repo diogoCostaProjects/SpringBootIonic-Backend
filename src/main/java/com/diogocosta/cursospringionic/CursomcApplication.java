0f0306c81a277747af2baaf62b8fab41e59821fd
diogoCostaProjects <diogocosta.jar@gmail.com>
2018-08-15 06:45:01 -0300

    Criando operação de instanciação

--- src/main/java/com/diogocosta/cursospringionic/CursomcApplication.java
+++ src/main/java/com/diogocosta/cursospringionic/CursomcApplication.java
@@ -1,12 +1,29 @@
 package com.diogocosta.cursospringionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.diogocosta.cursospringionic.domain.Categoria;
+import com.diogocosta.cursospringionic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }

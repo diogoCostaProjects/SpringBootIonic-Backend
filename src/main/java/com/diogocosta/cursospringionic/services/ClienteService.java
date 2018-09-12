f119e495064cc7386e1447dd306bb55a0dc9223b
diogoCostaProjects <diogocosta.jar@gmail.com>
2018-09-12 06:29:50 -0300

    Enpoint clientes/{id} disponivel

---
+++ src/main/java/com/diogocosta/cursospringionic/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.diogocosta.cursospringionic.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.diogocosta.cursospringionic.domain.Cliente;
+import com.diogocosta.cursospringionic.repositories.ClienteRepository;
+import com.diogocosta.cursospringionic.services.exceptions.ObjectNotFoundException;
+
+//classe de serviço para separação por camadas
+
+@Service  
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id){
+		Cliente obj = repo.findOne(id);
+		if(obj == null){
+				throw new ObjectNotFoundException("Objeto não encontrado Id: " + id +
+												",Tipo: "+Cliente.class.getName());
+		}
+		return obj;
+	}
+}

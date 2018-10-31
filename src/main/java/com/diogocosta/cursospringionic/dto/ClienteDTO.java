1e9afc72c4d2648f9c664661891eb1b96536b4a3
diogoCostaProjects <diogocosta.jar@gmail.com>
2018-10-31 07:00:41 -0200

    Validação customizada, e-mail não repetido na atualização de Cliente

--- src/main/java/com/diogocosta/cursospringionic/dto/ClienteDTO.java
+++ src/main/java/com/diogocosta/cursospringionic/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.diogocosta.cursospringionic.domain.Cliente;
+import com.diogocosta.cursospringionic.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
